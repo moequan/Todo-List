bf0124aa8ca678269bf2d039e579d76feb323652
moequan <maurice.s93@web.de>
2019-11-27 13:49:12 +0100

    Setting the route for the list

--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+  getTodo,
+  addTodo,
+  getTodo,
+  deleteTodo,
+  updateTodo
+} = require('../Controller/todoController');
+
+router
+  .route('/')
+  .get(getUsers)
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send({ name: 'Babylon' });
+  res.send({ name: 'MyToDo' });
 });
 
 module.exports = router;
